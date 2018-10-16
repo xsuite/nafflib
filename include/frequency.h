f7f49e43b8f172b92a55fecc21370f1d251dd2e1
kparasch <konstantinos.paraschou@cern.ch>
2018-10-16 15:21:04 +0200

    Completed python wrapper

--- include/frequency.h
+++ include/frequency.h
@@ -4,32 +4,20 @@
 #include <complex.h>
 #include <math.h>
 #include "signal_processing.h"
+#include "naffargs.h"
 #include "fft.h"
 #include "brent.h"
 #include "windows.h"
 
-//#define MAX_FREQS 1000
-//
-struct merit_args
-{
-    size_t N;
-    double _Complex* window;
-    double _Complex* signal;
-    
-//    double _Complex amplitude[MAX_FREQS];
-//    double  frequency[MAX_FREQS];
-//    int frequency_counter;
-//
-};
-
-typedef struct merit_args merit_args;
-
 double minus_magnitude_fourier_integral(double frequency, const merit_args* S);
 double get_f1(double _Complex* signal, size_t N, double order, double fft_estimate);
+double get_q(double _Complex* signal, size_t N, double order, int interpolate_integral);
 size_t interpolating_size(size_t N);
+void use_interpolating_integral_hardy(size_t N, double _Complex* window);
 void use_interpolating_integral(size_t N, double _Complex* window);
+void get_f(double _Complex* signal, size_t N, double order,  double* frequencies, double _Complex* amplitudes, double _Complex* negative_amplitudes, size_t n_freqs);
+void get_f_all(double _Complex* signal, size_t N, double order,  double* frequencies, double _Complex* amplitudes, size_t n_freqs);
 void get_f_neg(double _Complex* signal, size_t N, double order, double* frequencies, double _Complex* amplitudes, double _Complex* negative_amplitudes, size_t n_freqs);
 void py_f1(double _Complex* signal, size_t N, double order, double fft_estimate, double* tune);
 void pyget_f1(double _Complex* signal, size_t N, double order, double *tune);
-void get_tune(double _Complex* signal, size_t N, double order, double *tune);
 #endif
---
+++ include/toy1.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int a;
+    int b;
+    char win;
+
+
+}toy_data;
+
+void hello(int* N);
+void hello2(toy_data *data, const int N);
+void hi3(double array[], const int as);
+
+
+
+
