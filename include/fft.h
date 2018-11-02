cc2d88fb5bf144ed3f8f53e6983ebbb013c73bc4
kparasch <konstantinos.paraschou@cern.ch>
2018-11-02 18:59:29 +0100

    fix bug of including fftw3.h with COMPILE_WITHOUT_FFTW option

--- include/fft.h
+++ include/fft.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <complex.h>
 #include <stdlib.h>
-#include <fftw3.h>
 
 #define pi 3.141592653589793238462643383279
 
@@ -14,6 +13,8 @@ double max_fft_frequency(double _Complex* signal, size_t N);
 double _Complex* init_fft(double _Complex* signal, size_t N, size_t *M);
 
 #ifndef COMPILE_WITHOUT_FFTW
+#include <fftw3.h>
+
 double max_fftw_frequency_all(double _Complex* signal, size_t N);
 double max_fftw_frequency(double _Complex* signal, size_t N);
 #endif
