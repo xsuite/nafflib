28da7a292a5518bdb0b4dc3f89a8c3968211bc74
Konstantinos Paraschou <konstantinos.paraschou@cern.ch>
2018-11-27 23:25:40 +0100

    Fix python3 compatibility and add function to calculate tunes from array of particles' turn-by-turn data

--- __init__.py
+++ __init__.py
@@ -1,2 +1,11 @@
+import sys
+if sys.version_info[0] < 3:
+    from .NAFFlib2_c import *
+else:
+    from .NAFFlib_c import *
 
-from NAFFlib import *
+def multiparticle_tunes(x):
+    q_i = np.empty_like(x[:,0])
+    for ii in range(len(x)):
+        q_i[ii] = get_tune(x[ii])
+    return q_i
