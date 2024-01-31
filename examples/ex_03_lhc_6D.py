42073a6cdce838d5b94f20b15c646ad23131dcba
Philippe Bélanger <pbelanger@Philippes-Laptop.local>
2024-01-31 09:48:38 +0100

    finished LHC example

--- examples/ex_03_lhc_6D.py
+++ examples/ex_03_lhc_6D.py
@@ -26,7 +26,7 @@ def read_csv(filename):
 example_signals = []
 for file in files:
     data = read_csv(file)
-    data['name'] = file
+    data['set'] = ['on_momentum' if 'on_momentum' in file else 'off_momentum'][0]
     for plane in ['x','y','zeta']:
         z,pz = nafflib.generate_signal(data[f'A{plane}'],data[f'Q{plane}'],np.arange(num_turns))
         data[f'{plane}']  = z
@@ -36,51 +36,66 @@ for file in files:
     
 
 
-# TODO
 
+# Choosing number of harmonics and particls set
+particle_set = ['on_momentum','off_momentum'][1]
+num_harmonics = 50
 
-# fig, axes = plt.subplots(1, 3, figsize=(18, 6))
-# plt.suptitle('Particles on momentum')
-# for part in particles_on_p:
-#     for plane,ax in zip(['x','y','zeta'],axes):
-#         plt.sca(ax)
-#         plt.plot(part[plane],part[f'p{plane}'],'.',color='k',alpha=0.1)
+# Choosing turns to plot
+N_start = 0
+N_stop  = 100
 
-# # Adding labels
-# for plane,ax in zip(['x','y','zeta'],axes):
-#     plt.sca(ax)
-#     plt.axis('equal')
-#     if plane == 'zeta':
-#         plane = '\zeta'
-#     plt.xlabel(rf'$\tilde {plane}/\sqrt{{\varepsilon_{plane}}}$')
-#     plt.ylabel(rf'$\tilde p_{plane}/\sqrt{{\varepsilon_{plane}}}$')
+fig, axes = plt.subplots(1, 3, figsize=(18, 6))
+plt.suptitle(f'LHC, Particles {particle_set}')
+N = np.arange(num_turns)
+for data in example_signals:
+    # Choosing particles set
+    if data['set'] != particle_set:
+        continue
+
+    # Extracting fundamental frequencies
+    Q_vec = [nafflib.tune(data[f'{plane}'],data[f'p{plane}'],window_order=4) for plane in ['x','y','zeta']]
     
-#     if plane == '\zeta':
-#         plt.xlim(-0.5,0.5)
-#         plt.ylim(-0.5,0.5)
-#     else:
-#         plt.xlim(-6,6)
-#         plt.ylim(-6,6)
+    # Plotting plane-by-plane
+    for plane,ax in zip(['x','y','zeta'],axes):
+        plt.sca(ax)
+        A,Q      = data[f'A{plane}'],data[f'Q{plane}']
+        z_r,pz_r = data[f'{plane}'] ,data[f'p{plane}']
 
-# fig, axes = plt.subplots(1, 3, figsize=(18, 6))
-# plt.suptitle('Particles off momentum')
-# for part in particles_off_p:
-#     for plane,ax in zip(['x','y','zeta'],axes):
-#         plt.sca(ax)
-#         plt.plot(part[plane],part[f'p{plane}'],'.',color='k',alpha=0.1)
+        # Finding harmonics
+        A_r,Q_r = nafflib.harmonics(z_r,pz_r,num_harmonics, window_order=4)
 
-# # Adding labels
-# for plane,ax in zip(['x','y','zeta'],axes):
-#     plt.sca(ax)
-#     plt.axis('equal')
-#     if plane == 'zeta':
-#         plane = '\zeta'
-#     plt.xlabel(rf'$\tilde {plane}/\sqrt{{\varepsilon_{plane}}}$')
-#     plt.ylabel(rf'$\tilde p_{plane}/\sqrt{{\varepsilon_{plane}}}$')
+        # Reconstructing signal (original is already a reconstruction...)
+        z_rr,pz_rr = nafflib.generate_signal(A_r,Q_r,N)
+
+        # Plotting
+        plt.plot(z_r[N_start:N_stop] ,pz_r[N_start:N_stop],'o',mfc='none',color='C4' ,alpha=0.9)
+        plt.plot(z_rr[N_start:N_stop],pz_rr[N_start:N_stop],'.'   ,color='C0' ,alpha=0.9)
+
+        # If one wants to see the harmonics:
+        combin,err,freq = nafflib.find_linear_combinations(Q_r,fundamental_tunes= Q_vec)
+        # print(combin)
+
+        
+# Adding labels
+for plane,ax in zip(['x','y','zeta'],axes):
+    plt.sca(ax)
+
+    plt.plot(np.nan,np.nan,'o',mfc='none',color='C4' ,alpha=0.9,label='LHC trk (100 harmonics)')
+    plt.plot(np.nan,np.nan,'.',color='C0' ,alpha=0.9,label=f'nafflib ({num_harmonics} harmonics)')
+    plt.legend()
+
+    plt.axis('equal')
+    if plane == 'zeta':
+        plane = '\zeta'
+    plt.xlabel(rf'$\tilde {plane}/\sqrt{{\varepsilon_{plane}}}$')
+    plt.ylabel(rf'$\tilde p_{plane}/\sqrt{{\varepsilon_{plane}}}$')
     
-#     if plane == '\zeta':
-#         plt.xlim(-0.5,0.5)
-#         plt.ylim(-0.5,0.5)
-#     else:
-#         plt.xlim(-6,6)
-#         plt.ylim(-6,6)
+    if plane == '\zeta':
+        plt.xlim(-0.5,0.5)
+        plt.ylim(-0.5,0.5)
+    else:
+        plt.xlim(-7,7)
+        plt.ylim(-7,7)
+
+plt.show()
